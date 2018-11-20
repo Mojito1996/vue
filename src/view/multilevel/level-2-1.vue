e1af7b896972b820292f5333d578c39d1494e846
chengshuo <315891071@qq.com>
2018-11-20 15:43:30 +0800

    vue

--- src/view/multilevel/level-2-1.vue
+++ src/view/multilevel/level-2-1.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>多级菜单 -> 二级-1</div>
+  <div>
+    <Row>
+      <Col>
+        <img class="qq-group-img" :src="qqFans" alt="">
+      </Col>
+    </Row>
+  </div>
 </template>
 <script>
+import qqFans from '@/assets/images/qq-group1.jpg'
 export default {
-  name: 'level_2_1'
+  name: 'hello',
+  data () {
+    return {
+      msg: 'world'
+    }
+  }
 }
 </script>
